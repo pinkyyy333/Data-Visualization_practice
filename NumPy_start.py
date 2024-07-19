34d89eba905e86069962b7c1a6d7070d8acff3e8
Pinky <pineapple168.mg11@nycu.edu.tw>
2024-07-19 15:24:49 +0800

    基礎多維度運算功能

--- NumPy_start.py
+++ NumPy_start.py
@@ -28,7 +28,7 @@ data=np.empty([3,3])
 print(data)
 data=np.ones([2,3])
 print(data)
-'''
+
 #三維陣列
 data=np.array([
     [
@@ -53,3 +53,58 @@ data=np.array([
 print(data)
 data=np.ones([2,1,1,2])
 print(data)
+
+#矩陣運算
+#逐元運算(elementwise)
+data1=np.array([3,2,10])
+data2=np.array([1,3,6])
+result=data1+data2
+print("加法",result)
+result=data1*data2
+print("乘法",result)
+result=data1>data2
+print("大於",result)
+result=data1==data2
+print("是否相等",result)
+
+#矩陣運算(matrix)
+data1=np.array([
+    [1,3]
+]) #1*2
+data2=np.array([
+    [2,-1,3],
+    [-2,4,1]
+]) #2*3
+result=data1.dot(data2) #1*3
+result1=data1@data2
+print("內積",result)
+print("內積1",result1)
+result=np.outer(data1,data2)
+print("外積",result)
+
+#統計運算(statistics)
+# ndarray 多維陣列
+data=np.array([
+    [2,1,7],
+    [-5,3,8]
+]) #2*3
+
+result=data.sum()
+print("總和",result)
+result=data.max()
+print("最大值",result)
+result=data.mean()
+print("平均數",result)
+result=data.std()
+print("標準差",result)
+result=data.sum(axis=0) #針對欄位總和column(針對第一個相度做總和)
+print(result)
+result=data.sum(axis=1) #針對列位總和row(針對第二個相度做總和)
+print(result)
+result=data.max(axis=0)
+print(result)
+result=data.mean(axis=1)
+print(result)
+result=data.cumsum() #針對欄位總和column(針對第一個相度做逐數累加)
+print(result)
+'''
