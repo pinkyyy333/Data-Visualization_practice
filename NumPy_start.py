33f272bf5713c47643ae9079fb8abaabe3731c29
Pinky <pineapple168.mg11@nycu.edu.tw>
2024-07-19 16:07:47 +0800

    ndarray多維陣列的維度、形狀操作

--- NumPy_start.py
+++ NumPy_start.py
@@ -108,3 +108,47 @@ print(result)
 result=data.cumsum() #針對欄位總和column(針對第一個相度做逐數累加)
 print(result)
 '''
+#多維陣列維度/形狀操作
+#觀察資料形狀
+'''
+data=np.ones(10)
+print(data)
+print(data.shape)
+data=np.array([
+    [3,1,5],
+    [1,2,3]
+])
+print(data.shape)
+#資料轉置
+data=np.array([
+    [2,4,2],
+    [1,5,0]
+])
+print(data.shape)
+print(data.T)
+print(data.T.shape)
+'''
+#扁平化資料
+data=np.array([
+    [
+        [2,1,3],[1,2,3]
+    ],
+    [
+        [0,2,1],[8,9,10]
+    ]
+])
+newData=data.ravel()
+print(newData)
+print(newData.shape)
+#重塑資料形狀
+newData=data.reshape(3,4)
+print(newData)
+newData=data.reshape(1,2,6)
+print(newData)
+#初始化資料
+data=np.zeros(18).reshape(3,6)
+print(data)
+
+data=np.arange(9).reshape(3,3)
+print(data)
+print(data.T)
